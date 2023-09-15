59d6974b8aa8136b0e51125dc2d8057861c297be
nerioferrebus <nerio.ferrebuscastillo@unicolombo.edo.co>
2023-09-15 18:56:02 -0500

    Empezamos el proyecto de esta manera

---
+++ ProyectoAula/src/main/java/logica/logica.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package logica;
+import Igu.Login;
+
+/**
+ *
+ * @author Nerio Ferrebus
+ */
+public class logica {
+    public static void main(String[] args) {
+        
+    
+    Login inicio = new Login();
+    inicio.setVisible(true);
+    inicio.setLocationRelativeTo(null);
+    
+    
+    }
+}
