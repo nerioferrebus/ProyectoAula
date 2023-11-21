6f1dda4e5d1820f44ce2d50cf3aaa84d94f5c6af
EvertAlvis <evertalvis171@gmailcom>
2023-11-20 23:21:33 -0500

    ultima actualizacion

---
+++ src/proyecto/aula/Usuario.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package proyecto.aula;
+import java.util.*;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Usuario
+ */
+public class Usuario {
+    public String cedula;
+    public String nombre;
+    public String email;
+    public String telefono;
+    public String fecha;
+    public String habitacion;
+    public ArrayList<Servicio>listadeservicios;
+    public static HashMap<String,Usuario> usuarioBD;
+    
+}
